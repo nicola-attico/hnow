18ab156827c2ea95306d0726a452955b591e2731
Nicola Attico <nicola.attico@gmail.com>
2021-08-19 16:20:07 +0000

    modify deamon

--- deamon.sh
+++ deamon.sh
@@ -2,15 +2,27 @@
 
 while true; do
 
+echo "=== New Iteration ==="
+date
+
+echo "start"
+
 node listen.js $1 $2 $3 $4 $5 >> logs/listen_$1.log 2>&1 &
 
 pid=$!
 
-echo $pid >> logs/listen_$1.log
+echo $pid
 
-sleep 30
+ps -ef | grep listen.js | grep -v grep
 
-kill $pid
+sleep 60
+
+date
+echo "kill"
+
+kill -9 $pid
+
+ps -ef | grep listen.js | grep -v grep
 
 done
 
